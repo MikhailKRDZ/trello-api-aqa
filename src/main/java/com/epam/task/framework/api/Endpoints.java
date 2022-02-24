62f4fbfcf8e73cd0c7d55aa8a208d9acc7aee4a0
Mikhail Kirdzik <Mikhail_Kirdzik@epam.com>
2022-02-24 12:08:24 +0300

    add put Method

--- src/main/java/com/epam/task/framework/api/Endpoints.java
+++ src/main/java/com/epam/task/framework/api/Endpoints.java
@@ -8,4 +8,5 @@ public class Endpoints {
   public static final String TRELLO = "https://api.trello.com";
   public static final String BOARDS = TRELLO + "/1/boards/";
   public static final String BOARDS_NAME = TRELLO + "/1/boards?name=";
+  public static final String NAME = "/?name=";
 }
--- src/test/java/com/epam/task/TrelloBoardCreatedTest.java
+++ src/test/java/com/epam/task/TrelloBoardCreatedTest.java
@@ -3,8 +3,10 @@ package com.epam.task;
 
 import static com.epam.task.framework.api.Endpoints.BOARDS;
 import static com.epam.task.framework.api.Endpoints.BOARDS_NAME;
+import static com.epam.task.framework.api.Endpoints.NAME;
 import static com.epam.task.framework.service.UserCreator.withCredentialsFromProperty;
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
 
 import java.util.Map;
 import com.epam.task.framework.model.Board;
@@ -18,12 +20,14 @@ public class TrelloBoardCreatedTest {
   private final String queryParameterKey = "6ba72479ab2fc4cd11e1743548851efe";
   private final String token = "token";
   private final String queryParameterToken = "23c6a2ffe0a31e76ce03d1ae6256189cbf8675879cb40b2ed99b9b247d9f419f";
-  private final String expectedBoardsName = "Boards with api and intellij";
+  private final String expectedBoardsCreatedName = "Boards with api and intellij";
+  private final String expectedBoardsChangesName = "Boards changes with api and intellij";
   private final String defaultListFalse = "&defaultList=false";
   private final Map<String, String> userData = withCredentialsFromProperty();
   private Board actualBoard;
+  private String boardId;
 
-  @Test
+  @Test(priority = 1)
   public void testBoardCreated() {
     actualBoard = given()
         .log()
@@ -31,22 +35,39 @@ public class TrelloBoardCreatedTest {
         .queryParam(key, queryParameterKey)
         .queryParam(token, queryParameterToken)
         .body(userData)
-        .post(BOARDS_NAME + expectedBoardsName + defaultListFalse)
+        .post(BOARDS_NAME + expectedBoardsCreatedName + defaultListFalse)
         .then()
         .statusCode(200)
         .extract()
         .as(Board.class);
     SoftAssert softAssert = new SoftAssert();
-    softAssert.assertEquals(actualBoard.getName(), expectedBoardsName,
+    softAssert.assertEquals(actualBoard.getName(), expectedBoardsCreatedName,
         "New Boards name isn't equal expected!");
     softAssert.assertNotEquals(actualBoard.getId(), null,
         "New Boards id isn't created!");
     softAssert.assertAll();
   }
 
+  @Test(priority = 2)
+  public void testBoardChangesName() {
+    boardId = actualBoard.getShortUrl().split("/")[4];
+    actualBoard = given()
+        .log()
+        .method()
+        .queryParam(key, queryParameterKey)
+        .queryParam(token, queryParameterToken)
+        .body(userData)
+        .put(BOARDS + boardId + NAME + expectedBoardsChangesName)
+        .then()
+        .statusCode(200)
+        .extract()
+        .as(Board.class);
+    assertEquals(actualBoard.getName(), expectedBoardsChangesName,
+        "Changes Boards name isn't equal expected!");
+  }
+
   @AfterTest
   public void deleteBoard() {
-    String boardId = actualBoard.getShortUrl().split("/")[4];
     given()
         .log()
         .method()
